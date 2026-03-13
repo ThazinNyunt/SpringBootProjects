24eba120bd0352b8acddf1aea18cd58e3ef3db2b
Thazin Nyunt <thazinnyunt06@gmail.com>
2026-03-13 12:15:34 +0630

    lombok remove

--- MobileBroker/src/main/java/com/innoveller/smsbroker/services/data/Ndc.java
+++ MobileBroker/src/main/java/com/innoveller/smsbroker/services/data/Ndc.java
@@ -13,23 +13,11 @@ public class Ndc {
         return ndc;
     }
 
-    public void setNdc(Integer ndc) {
-        this.ndc = ndc;
-    }
-
     public String getServiceArea() {
         return serviceArea;
     }
 
-    public void setServiceArea(String serviceArea) {
-        this.serviceArea = serviceArea;
-    }
-
     public String getNumberType() {
         return numberType;
-    }
-
-    public void setNumberType(String numberType) {
-        this.numberType = numberType;
     }
 }
--- MobileBroker/src/main/java/com/innoveller/smsbroker/services/data/OperatorPrefix.java
+++ MobileBroker/src/main/java/com/innoveller/smsbroker/services/data/OperatorPrefix.java
@@ -14,31 +14,15 @@ public class OperatorPrefix {
         return operator;
     }
 
-    public void setOperator(String operator) {
-        this.operator = operator;
-    }
-
     public Integer getNdc() {
         return ndc;
-    }
-
-    public void setNdc(Integer ndc) {
-        this.ndc = ndc;
     }
 
     public Integer getPrefixStart() {
         return prefixStart;
     }
 
-    public void setPrefixStart(Integer prefixStart) {
-        this.prefixStart = prefixStart;
-    }
-
     public Integer getPrefixEnd() {
         return prefixEnd;
-    }
-
-    public void setPrefixEnd(Integer prefixEnd) {
-        this.prefixEnd = prefixEnd;
     }
 }
