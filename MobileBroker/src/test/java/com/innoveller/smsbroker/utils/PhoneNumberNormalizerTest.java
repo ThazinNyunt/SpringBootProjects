e4da36e5982eb7cc43ddaf4f6a92952dc87d548a
Thazin Nyunt <thazinnyunt06@gmail.com>
2026-03-13 17:08:55 +0630

    PrefixCache NdcIfo OperatorInfo PhoneNumberNormalizer PhoneNumberInfoServiceImpl  PhoneNumberNormalizerTest

--- MobileBroker/src/test/java/com/innoveller/smsbroker/utils/PhoneNumberNormalizerTest.java
+++ MobileBroker/src/test/java/com/innoveller/smsbroker/utils/PhoneNumberNormalizerTest.java
@@ -1,11 +1,27 @@
 package com.innoveller.smsbroker.utils;
 
+import com.innoveller.smsbroker.exceptions.PhoneNumberInfoLookupError;
 import io.vavr.control.Either;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PhoneNumberNormalizerTest {
 
-    void shouldReturnErrorWhenPhoneIsNull() {
+    @Test
+    void shouldReturnNsnForValidMyanmarPhoneNumber() {
+        Either<PhoneNumberInfoLookupError, String> result = PhoneNumberNormalizer.getNationalSignificantNumber("+959254252784");
 
+        assertTrue(result.isRight());
+        assertEquals("9254252784", result.get());
+    }
 
+    @Test
+    void shouldConvertNumberStartWithZero() {
+        Either<PhoneNumberInfoLookupError, String> result = PhoneNumberNormalizer.getNationalSignificantNumber("09254252784");
+
+        assertTrue(result.isRight());
+        assertEquals("9254252784", result.get());
     }
 }
